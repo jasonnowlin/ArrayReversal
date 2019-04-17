a9a5a3d3562dd1867a7982c7117b1ebbcda6b493
Jason Nowlin <jnowlin@ebsco.com>
2019-04-17 10:45:19 -0500

    add null and empty array testing

--- src/ArrayReversal/ArrayReversal/ArrayUtil.cs
+++ src/ArrayReversal/ArrayReversal/ArrayUtil.cs
@@ -6,29 +6,82 @@ namespace ArrayReversal
     {
         public static int[] reverseLikeNoob(int[] arr)
         {
+            if (arr == null)
+            {
+                return null;
+            }
+
             for (var i = 0; i < arr.Length / 2; i++)
             {
                 var temp = arr[i];
                 arr[i] = arr[arr.Length - 1 - i];
                 arr[arr.Length - 1 - i] = temp;
             }
+
             return arr;
         }
 
         public static int[] reverseLikePro(int[] arr)
         {
+            if (arr == null)
+            {
+                return null;
+            }
+
             for (var i = 0; i < arr.Length / 2; i++)
             {
                 arr[i] = arr[i] ^ arr[arr.Length - 1 - i] ^ (arr[arr.Length - 1 - i] = arr[i]);
             }
+
             return arr;
         }
 
         public static int[] reverseLikeZenMaster(int[] arr)
         {
+            if (arr == null)
+            {
+                return null;
+            }
+
             // Someone already solved this problem, next task please!
             Array.Reverse(arr);
+
             return arr;
+        }
+
+        public static int[] reverseLikeMicrosoft(int[] arr)
+        {
+            if (arr == null)
+            {
+                return null;
+            }
+
+            var i = 0;
+            var j = arr.Length - 1;
+            while (i < j)
+            {
+                var temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                i++;
+                j--;
+            }
+
+            return arr;
+        }
+
+        public static int[] copyAndReverse(int[] arr)
+        {
+            if (arr == null)
+            {
+                return null;
+            }
+
+            var copy = new int[arr.Length];
+            Array.Copy(arr, copy, arr.Length);
+            Array.Reverse(copy);
+
+            return copy;
         }
     }
 }
---
+++ src/ArrayReversal/ArrayReversalTests/GivenEmptyArray.cs
@@ -0,0 +1,53 @@
+﻿using ArrayReversal;
+using NUnit.Framework;
+
+namespace ArrayReversalTests
+{
+    public class GivenEmptyArray
+    {
+        private int[] _input;
+        private int[] _expected;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _input = new int[0];
+            _expected = new int[0];
+        }
+
+        [Test]
+        public void WhenIamNoob()
+        {
+            var actual = ArrayUtil.reverseLikeNoob(_input);
+            Assert.AreEqual(_expected, actual);
+        }
+
+        [Test]
+        public void WhenIamPro()
+        {
+            var actual = ArrayUtil.reverseLikePro(_input);
+            Assert.AreEqual(_expected, actual);
+        }
+
+        [Test]
+        public void WhenIamZenMaster()
+        {
+            var actual = ArrayUtil.reverseLikeZenMaster(_input);
+            Assert.AreEqual(_expected, actual);
+        }
+
+        [Test]
+        public void WhenIamMicrosoft()
+        {
+            var actual = ArrayUtil.reverseLikeMicrosoft(_input);
+            Assert.AreEqual(_expected, actual);
+        }
+
+        [Test]
+        public void WhenICopyAndReverse()
+        {
+            var actual = ArrayUtil.copyAndReverse(_input);
+            Assert.AreEqual(_expected, actual);
+        }
+    }
+}
